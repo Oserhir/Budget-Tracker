5eed77ac6ac16eb753ebf3087188a9e9a1a36391
OSERHIRDEV <oserhir10@gmail.com>
2022-08-16 14:05:00 +0100

    Display Table

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
- 
+const table = document.getElementById("table");
+const Name = document.getElementById("Name").value;
+const Date = document.getElementById("Date").value;
+const Amount = document.getElementById("Amount").value;
+
+console.log(`${Name} ${Date} ${Amount}`);
+
+let Array = [];
+
+let myArr = [
+  { id: 1, name: "mcdonald", date: "07/12/2024", Amount: 30 },
+  { id: 2, name: "Mila", date: "07/12/2024", Amount: 40 },
+];
+
+let DisplayTable = () => {
+  let Afficher = "";
+  for (let i = 0; i < myArr.length; i++) {
+    Afficher += `   <tr>
+    <th scope="row">${myArr[i]["id"]}</th>
+    <td> ${myArr[i]["name"]} </td>
+    <td>${myArr[i]["date"]}</td>
+    <td> ${myArr[i]["Amount"]} </td>
+  </tr>`;
+  }
+
+  table.innerHTML = Afficher;
+};
+
+DisplayTable();
