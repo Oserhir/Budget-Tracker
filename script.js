e9e1d715d30138f7ea883f743e6e1fe35741c482
OSERHIRDEV <oserhir10@gmail.com>
2022-08-16 16:41:39 +0100

    Transaction History

--- script.js
+++ script.js
@@ -1,36 +1,78 @@
-const table = document.getElementById("table");
+const table = document.getElementById("transaction");
 const Name = document.getElementById("Name");
-const Date = document.getElementById("Date");
+const balance = document.getElementById("balance");
 const Amount = document.getElementById("Amount");
 const AddExpense = document.querySelector("Button");
 
-let Arrays = [];
+window.onload = () => {};
+
+let transactions = [];
 
 AddExpense.addEventListener("click", () => {
   AddData();
   DisplayData();
+  displayYourBalance();
 });
 
 let AddData = () => {
-  let Data = {
-    id: Arrays.length + 1,
+  let transaction = {
+    id: generateID(),
     name: Name.value,
-    date: Date.value,
+    date: currentDate(),
     amount: Amount.value,
   };
-  Arrays.push(Data);
+  transactions.push(transaction);
+
+  Name.value = "";
+  Amount.value = "";
+};
+
+// Generate random ID
+let generateID = () => {
+  return Math.floor(Math.random() * 100000000);
+};
+// Generate current DATE
+let currentDate = () => {
+  const date = new Date();
+  let currentDate = `${date.getDate()}-${
+    date.getMonth() + 1
+  }-${date.getFullYear()}`;
+  return currentDate;
 };
 
 let DisplayData = () => {
   let Data = "";
-  const result = Arrays.map((item) => {
-    Data += `<tr>
-    <th scope="row">${item["id"]}</th>
-    <td> ${item["name"]} </td>
-    <td>${item["date"]}</td>
-    <td> ${item["amount"]} </td>
-  </tr>`;
+  const result = transactions.map((item) => {
+    Data += `<li>
+    ${item["name"]}
+    <div>
+      <span class= ${
+        Number(item["amount"]) > 0 ? "income" : "expense"
+      }  > $${Math.abs(Number(item["amount"]))}  </span>
+        
+    </div>
+  </li> `;
+  });
+  // <span class="material-symbols-outlined"> delete </span>
+  table.innerHTML = Data;
+};
+
+let displayYourBalance = () => {
+  balance.innerHTML = `$${yourBalance()}`;
+};
+
+let yourBalance = () => {
+  let sum = 0;
+
+  transactions.map((item) => {
+    let Amount = Number(item["amount"]);
+    console.log(Amount);
+    if (Amount > 0) {
+      sum = sum + Amount;
+    } else {
+      sum = sum - Amount;
+    }
   });
 
-  table.innerHTML = Data;
+  return sum.toFixed(2);
 };
